0f7526f9dd8b999716a2efa5bdc6588a77c890c3
David Overman <doverman@uci.edu>
2023-10-16 13:05:14 -0700

    Initial Commit

---
+++ Minesweeper/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Minesweeper
+//
+//  Created by David Overman on 10/16/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
